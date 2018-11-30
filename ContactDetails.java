62b42e8a148ad1b50d98bbc7621ac7f6d1d76302
parimi1230 <parimi1230@gmail.com>
2018-11-30 10:52:38 +0530

    Add files via upload

--- ContactDetails.java
+++ ContactDetails.java
@@ -1,14 +1,10 @@
 package com.sample.dependencyinjection;
 
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class ContactDetails {
 
     private String phone;
     private String email;
-    private int distance;
 
     public String getPhone() {
         return phone;
@@ -24,21 +20,5 @@ public class ContactDetails {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-
-    @Override
-    public String toString() {
-        return "ContactDetails{" +
-                "phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                '}';
     }
 }
--- Employee.java
+++ Employee.java
@@ -15,38 +15,6 @@ public class Employee {
         this.height = height;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public IAddress getAddress() {
-        return address;
-    }
-
-    public void setAddress(IAddress address) {
-        this.address = address;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     @Override
     public String toString() {
         return "Employee{" +
